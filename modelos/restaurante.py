7868e7a7c7b3bc5c7ec66469053db3da5a109643
LuisFelipeSantana64 <luisfelipesantana64@gmail.com>
2025-01-29 11:48:41 -0300

    criando o construtor e um metodo para listar

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,12 +1,19 @@
-class Restaurantes:
+class Restaurante:
+    restaurantes = []
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.estado_ativo = False
+        Restaurante.restaurantes.append(self)
 
-    def __str__(this):
-        return f'\n{this.nome} | {this.categoria}'
+    def __str__(self):
+        return f'\n{self.nome} | {self.categoria} | {self.estado_ativo}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.estado_ativo}')
 
-restaurante1 = Restaurantes('Praça', 'Gourmet')
+restaurante1 = Restaurante('Praça', 'Gourmet')
 
+Restaurante.listar_restaurantes()
 print(restaurante1)
