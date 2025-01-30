ec9362964484f207d0874c249b74fbb9739fa1b0
LuisFelipeSantana64 <luisfelipesantana64@gmail.com>
2025-01-30 15:01:21 -0300

    usando property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,19 +1,47 @@
 class Restaurante:
     restaurantes = []
+ 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.estado_ativo = False
+        self.__nome = nome.title()
+        self.__categoria = categoria.title()
+        self.__ativo = False
         Restaurante.restaurantes.append(self)
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'NOME RESTAURANTE'.ljust(25)} | {'CATEGORIA'.ljust(25)} | {'ATIVADO'.ljust(25) }\n')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante.__nome.ljust(25)} | {restaurante.__categoria.ljust(25)} | {restaurante.ativo}')
 
     def __str__(self):
-        return f'\n{self.nome} | {self.categoria} | {self.estado_ativo}'
+        return f'\n{self.__nome} | {self.__categoria} | {self.ativo}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.estado_ativo}')
+    def alterar_estado(self):
+        print(f"\033[1;34m \n\tALTERANDO ESTADO\033[0m")
+        if self.__ativo == False:
+            self.__ativo = not self.__ativo
+            print(f"O restaurante'\033[92m{self.__nome}\033[0m'foi ativado com sucesso!!")
+        else:
+            try:
+                decisao = int(input(f"'{self.__nome}' ja está ativado, deseja desativar? (1 para sim):"))
+                if decisao == 1:
+                    self.__ativo = not self.__ativo
+                    print(f"\nO restaurante '{self.__nome}' foi desativado com sucesso!!")
+                else:
+                    print("\nNão houve mudança!")
+            except: 
+                print("\nNão houve mudança!")
+
+        
+    @property #getter
+    def ativo(self): 
+        return '☑'if self.__ativo else '☐' 
 
 restaurante1 = Restaurante('Praça', 'Gourmet')
 
 Restaurante.listar_restaurantes()
-print(restaurante1)
+restaurante1.alterar_estado()
+Restaurante.listar_restaurantes()
+
+
+
